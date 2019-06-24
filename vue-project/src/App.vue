7f9ed1859576181b08b0928bca4ee32901f757cf
Alex Harlamov <halex.alex@yandex.ru>
2019-06-24 16:59:22 +0300

    Create navigaion

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,10 +1,31 @@
 <template>
   <div class="container">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="collapse navbar-collapse">
+        <ul class="navbar-nav">
+          <!-- <li class="nav-item">
+            <router-link class="nav-link" :to="'/'">Home</router-link>
+          </li> -->
+          <router-link tag="li" class="nav-item" exact to="/" active-class="active">
+            <a class="nav-link">Home</a>
+          </router-link>
+          <!-- <li class="nav-item active">
+            <router-link class="nav-link" :to="'/pizza'">Pizza</router-link>
+          </li> -->
+          <router-link tag="li" class="nav-item" to="/pizza" active-class="active">
+            <a class="nav-link">Pizza</a>
+          </router-link>
+        </ul>
+      </div>
+    </nav>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+export default {
+
+}
 </script>
 
 <style scoped>
